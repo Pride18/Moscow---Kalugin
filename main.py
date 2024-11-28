a4505c00259bd3c1c32995ec1f80e3ca064d046b
Proud18 <tim575604@gmail.com>
2024-11-29 00:21:51 +0300

    I did the second task

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from random import randint
+
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw(self, qp):
+        r = randint(50, 250)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(0, 0, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
